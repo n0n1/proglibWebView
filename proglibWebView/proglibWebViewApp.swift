9989ea9386118ddd78c7293fd4947fb545be45e3
Ivan Telenkov <n0n1.vensy@gmail.com>
2021-03-30 14:16:19 +0300

    Initial Commit

---
+++ proglibWebView/proglibWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  proglibWebViewApp.swift
+//  proglibWebView
+//
+//  Created by Ivan Telenkov on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct proglibWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
