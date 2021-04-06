f77ad3aa992b605842cdc1cf1be1e8208045c883
Ivan Telenkov <n0n1.vensy@gmail.com>
2021-04-06 22:17:57 +0300

    LoaderView Init

---
+++ proglibWebView/LoaderView.swift
@@ -0,0 +1,20 @@
+//
+//  LoaderView.swift
+//  proglibWebView
+//
+//  Created by Ivan Telenkov on 06.04.2021.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoaderView()
+    }
+}
