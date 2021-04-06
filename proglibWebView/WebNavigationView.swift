53e467eb7adad4f4892cf29825bdfe692176e1e0
Ivan Telenkov <n0n1.vensy@gmail.com>
2021-04-06 20:27:39 +0300

    WebNavigationView Init

---
+++ proglibWebView/WebNavigationView.swift
@@ -0,0 +1,20 @@
+//
+//  WebNavigationView.swift
+//  proglibWebView
+//
+//  Created by Ivan Telenkov on 04.04.2021.
+//
+
+import SwiftUI
+
+struct WebNavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebNavigationView()
+    }
+}
