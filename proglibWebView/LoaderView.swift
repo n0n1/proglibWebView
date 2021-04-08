d4833678fc0464e62e1d98f0508f4f8400bab79c
Ivan Telenkov <n0n1.vensy@gmail.com>
2021-04-08 18:39:09 +0300

    added implementation of LoaderView

--- proglibWebView/LoaderView.swift
+++ proglibWebView/LoaderView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct LoaderView: View {
+	@State var isSpinCircle = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+		ZStack {
+			Circle()
+				.frame(width: 60, height: 60, alignment: .center)
+			VStack {
+				Circle()
+					.trim(from: 0.3, to: 1)
+					.stroke(Color.white, lineWidth: 2)
+					.frame(width:50, height: 50)
+					.padding(.all, 8)
+					.rotationEffect(.degrees(isSpinCircle ? 0 : -360), anchor: .center)
+					.animation(Animation.linear(duration: 0.6).repeatForever(autoreverses: false))
+					.onAppear {
+						self.isSpinCircle = true
+					}
+			}
+		}
+	}
 }
 
 struct LoaderView_Previews: PreviewProvider {
